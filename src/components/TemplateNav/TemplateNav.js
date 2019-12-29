c9f80737b30815705775b7c1f518d76511e06a97
ajedelmann <albert.edelmann@gmail.com>
2019-12-29 23:39:23 +0100

    added buttons on template nav for design and services

--- src/components/TemplateNav/TemplateNav.js
+++ src/components/TemplateNav/TemplateNav.js
@@ -9,54 +9,75 @@ class TemplateNav extends React.Component {
         <div className="design">
           <div className="tool-label">Design</div>
           <div className="tools">
-            <span className="tool-icon first-icon">
-              <img
-                src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/text-icon-18-256.png"
-                alt="text icon"
-                title="text icon"
-                className="template-icon"
-              />
-            </span>
-            <span className="tool-desc">Font</span>
-            <span className="tool-icon">
-              <img
-                src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/brush-icon-18-256.png"
-                alt="brush icon"
-                title="brush icon"
-                className="template-icon"
-              />
-            </span>
-            <span className="tool-desc">Theme</span>
-            <span className="tool-icon">☷</span>
-            <span className="tool-desc">Layout</span>
-            <span className="tool-icon">❏</span>
-            <span className="tool-desc">Template</span>
+            <button className="font-btn">
+              <span className="tool-icon first-icon">
+                <img
+                  src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/text-icon-18-256.png"
+                  alt="text icon"
+                  title="text icon"
+                  className="template-icon"
+                />
+              </span>
+              <span className="tool-desc">Font</span>
+            </button>
+            <button className="theme-btn">
+              <span className="tool-icon">
+                <img
+                  src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/brush-icon-18-256.png"
+                  alt="brush icon"
+                  title="brush icon"
+                  className="template-icon"
+                />
+              </span>
+              <span className="tool-desc">Theme</span>
+            </button>
+            <button className="layout-btn">
+              <span className="tool-icon">☷</span>
+              <span className="tool-desc">Layout</span>
+            </button>
+            <button className="template-btn">
+              <span className="tool-icon">❏</span>
+              <span className="tool-desc">Template</span>
+            </button>
           </div>
         </div>
         <div className="utilities">
           <div className="tool-label">Utilities</div>
           <div className="tools">
-            <span className="tool-icon first-icon">❰</span>
-            <span className="tool-icon">❱</span>
+            <button className="back-btn">
+              <span className="tool-icon">❰</span>
+            </button>
+            <button className="fwd-btn">
+              <span className="tool-icon">❱</span>
+            </button>
           </div>
         </div>
+
         <div className="download">
-          <div className="tools">
-            <span className="tool-icon first-icon">⤓</span>
-            <span className="tool-desc tool-download">Download</span>
-          </div>
+          <button className="download-btn">
+            <div className="tools">
+              <span className="tool-icon first-icon">⤓</span>
+              <span className="tool-desc tool-download">Download</span>
+            </div>
+          </button>
         </div>
+
         <div className="my-documents">
-          <div className="tools">
-            <span className="tool-icon tool-download">My Documents</span>
-          </div>
+          <button className="my-docs-btn">
+            <div className="tools">
+              <span className="tool-icon tool-mydoc">My Documents</span>
+            </div>
+          </button>
         </div>
+
         <div className="template-menu">
-          <div className="tools temp-menu">
-            <span className="tool-menu-bar first-icon">―</span>
-            <span className="tool-menu-bar first-icon">―</span>
-            <span className="tool-menu-bar first-icon">―</span>
-          </div>
+          <button className="template-menu-btn">
+            <div className="tools temp-menu">
+              <span className="tool-menu-bar first-icon">―</span>
+              <span className="tool-menu-bar first-icon">―</span>
+              <span className="tool-menu-bar first-icon last-bar">―</span>
+            </div>
+          </button>
         </div>
       </div>
     );
