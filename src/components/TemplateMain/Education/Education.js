b101631ae21e05a800a7fa96d33f7cb4f01f0142
ajedelmann <albert.edelmann@gmail.com>
2020-01-03 18:13:14 +0100

    changed cv header to be editable on click

--- src/components/TemplateMain/Education/Education.js
+++ src/components/TemplateMain/Education/Education.js
@@ -1,14 +1,37 @@
 import React from "react";
 
 class Education extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange = event => {
+    return event.target.value;
+  };
+
   render() {
     return (
       <div className="education">
         <div className="education-label">EDUCATION</div>
         <div className="education-body">
           <div className="education-group">
-            <div className="subject">Full Stack Web Development</div>
-            <div className="institution">DCI - Digital Career Institute</div>
+            <div>
+              <input
+                className="subject"
+                type="text"
+                defaultValue={"Full Stack Web Development"}
+                onClick={this.handleTextChange}
+              ></input>
+            </div>
+            <div>
+              <input
+                className="institution"
+                type="text"
+                defaultValue={"DCI - Digital Career Institute"}
+                onClick={this.handleTextChange}
+              ></input>
+            </div>
             <div className="time-loc">
               <div className="period">03/2019 - 04/2020</div>
               <div className="location">Berlin, Germany</div>
--- src/components/TemplateMain/TemplateHeader/TemplateHeader.js
+++ src/components/TemplateMain/TemplateHeader/TemplateHeader.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 class TemplateHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange = event => {
+    return event.target.value;
+  };
+
   render() {
     return (
       <div className="header">
@@ -15,14 +24,30 @@ class TemplateHeader extends React.Component {
             />
           </div>
           <div className="introduction">
-            <div className="name">Babylonian Dog</div>
-            <div className="title">Full Stack Dog</div>
-            <div className="summary">
-              Lorizzle ipsum dolor sit gangster, pot adipiscing elit. Nullam
-              shizznit velizzle, sizzle volutpat, suscipizzle quis, black
-              vizzle, shut the shizzle up. Pellentesque dope tortizzle. Sed
-              eros. Shizzlin dizzle at dolizzle dapibizzle turpizzle tempizzle
-              hizzle.
+            <div>
+              <input
+                className="name"
+                type="text"
+                defaultValue={"Babylonian Dog"}
+                onClick={this.handleTextChange}
+              ></input>
+            </div>
+            <div>
+              <input
+                className="title"
+                type="text"
+                defaultValue={"Full Stack Dog"}
+                onClick={this.handleTextChange}
+              ></input>
+            </div>
+            <div>
+              <textarea
+                className="summary"
+                placeholder="Lorizzle ipsum dolor sit gangster, pot adipiscing elit. Nullam shizznit velizzle, sizzle volutpat, suscipizzle quis, blackvizzle, shut the shizzle up. Pellentesque dope tortizzle. Sederos. Shizzlin dizzle at dolizzle dapibizzle turpizzle tempizzlehizzle."
+                cols="90"
+                rows="5"
+                onClick={this.handleTextChange}
+              ></textarea>
             </div>
           </div>
         </div>
