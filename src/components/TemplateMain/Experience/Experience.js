5207f88b254040f28670336e2c8b253a8e594b13
ajedelmann <albert.edelmann@gmail.com>
2020-01-03 19:15:48 +0100

    changed experience section to be editable on click

--- src/components/TemplateMain/Experience/Experience.js
+++ src/components/TemplateMain/Experience/Experience.js
@@ -1,24 +1,103 @@
 import React from "react";
 
 class Experience extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange = event => {
+    return event.target.value;
+  };
+
   render() {
     return (
       <div className="experience">
         <div className="experience-label">WORK EXPERIENCE</div>
         <div className="experience-body">
           <div className="experience-group">
-            <div className="role">Title/function</div>
-            <div className="company">Company</div>
+            <div>
+              <input
+                className="role"
+                type="text"
+                defaultValue={"Title/function"}
+                onClick={this.handleTextChange}
+              ></input>
+            </div>
+            <div>
+              <input
+                className="company"
+                type="text"
+                defaultValue={"Company"}
+                onClick={this.handleTextChange}
+              ></input>
+            </div>
             <div className="time-loc">
-              <div className="period">date</div>
+              <div>
+                <input
+                  className="period"
+                  type="text"
+                  defaultValue={"mm / yyyy  -  mm / yyyy"}
+                  onClick={this.handleTextChange}
+                ></input>
+              </div>
               <div className="location">City, Country</div>
             </div>
             <div className="achiev-task">Achivements / Tasks</div>
-            <div className="task-list">
-              <span>- </span>Curabitur fo yo mamma fizzle nisi hizzle mollis.
-              Shiznit you son of a bizzle. Shizznit odio. Vivamus neque. Fo
-              shizzle orci. For sure mauris crackalackin, interdum boofron, pot
-              sit amizzle.
+            <div>
+              <ul className="task-ul">
+                <li>
+                  <input
+                    className="task-list"
+                    type="text"
+                    defaultValue={"Short description of a task"}
+                    onClick={this.handleTextChange}
+                  ></input>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="experience-group">
+            <div>
+              <input
+                className="role"
+                type="text"
+                defaultValue={"Title/function"}
+                onClick={this.handleTextChange}
+              ></input>
+            </div>
+            <div>
+              <input
+                className="company"
+                type="text"
+                defaultValue={"Company"}
+                onClick={this.handleTextChange}
+              ></input>
+            </div>
+            <div className="time-loc">
+              <div>
+                <input
+                  className="period"
+                  type="text"
+                  defaultValue={"mm / yyyy  -  mm / yyyy"}
+                  onClick={this.handleTextChange}
+                ></input>
+              </div>
+              <div className="location">City, Country</div>
+            </div>
+            <div className="achiev-task">Achivements / Tasks</div>
+            <div>
+              <ul className="task-ul">
+                <li>
+                  <input
+                    className="task-list"
+                    type="text"
+                    defaultValue={"Short description of a task"}
+                    onClick={this.handleTextChange}
+                  ></input>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
