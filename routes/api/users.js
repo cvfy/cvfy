1902b16e41e0707a8b3b4e3a5e15771efdc4e737
AlexandrMel <melnic.alexandr88@gmail.com>
2020-01-04 21:59:00 +0100

    added Register User route and configured Passport.js - JWT Strategy

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,3 +8,37 @@ const keys = require("../../config/keys");
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");// Load User model
 const User = require("../../models/User");
+
+// Load User model
+const User = require("../../models/User");
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post("/register", (req, res) => {
+    // Form validation 
+    const { errors, isValid } = validateRegisterInput(req.body); // Check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        return res.status(400).json({ email: "Email already exists" });
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });// Hash password before saving in database
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  });
