de2fb83355b4154ea5fe8095e9bd7937081b67c7
AlexandrMel <melnic.alexandr88@gmail.com>
2020-01-05 13:18:08 +0100

    The MERN Authentification set up finished

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-import errorReducer from "./errorReducer";
 
-export default combineReducers({
-  auth: authReducer,
-  errors: errorReducer
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
@@ -9,8 +8,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose
   )
 );
-
 export default store;
