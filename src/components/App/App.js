5db2046f4dfe6b9890cadac2446ff19a9ac8bdeb
AlexandrMel <melnic.alexandr88@gmail.com>
2020-01-21 16:57:48 +0100

    Finished cleaning up the footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,15 @@
-import React, { useState } from 'react';
-import Navbar from '../navbar/Navbar';
-import '../navbar/Navbar.css'
-import Jumbotron from '../Jumbotron'
-import '../Jumbotron.css'
+import React from 'react';
 import './App.css'
 import '../../styles/animate.css'
 import Footer from '../Footer'
 import '../Footer.css'
 
 import GlobalStyle from '../../styles/Global'
-import CV_Templates from '../CV_Templates';
 
 const App = () => {
 
   return (
     <div className="MainPage">
-      <Navbar />
-      <Jumbotron />
-      <CV_Templates />
       <Footer />
       <GlobalStyle />
       </div>
--- src/components/navbar/Brand.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Brand = () => {
-  return (
-    <div className="Logo"><h1>CV|FY</h1></div>
-  )
-}
-
-export default Brand
