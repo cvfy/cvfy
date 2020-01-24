d330affcb8c7eb322bc32ca21ec21775587bcd70
ajedelmann <albert.edelmann@gmail.com>
2020-01-24 14:31:13 +0100

    added Register component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import Navbar from '../navbar/Navbar';
-import '../navbar/Navbar.css'
-import Jumbotron from '../Jumbotron'
-import '../Jumbotron.css'
-import './App.css'
-import '../../styles/animate.css'
-import Footer from '../Footer'
-import '../Footer.css'
+import React from "react";
+import Navbar from "../navbar/Navbar";
+import "../navbar/Navbar.css";
+import Jumbotron from "../Jumbotron";
+import "../Jumbotron.css";
+import "./App.css";
+import "../../styles/animate.css";
+import Footer from "../Footer";
+import "../Footer.css";
+import Register from "../Register/Register";
 
-import GlobalStyle from '../../styles/Global'
+import GlobalStyle from "../../styles/Global";
 
 const App = () => {
-
   return (
     <div className="MainPage">
       <Navbar />
       <Jumbotron />
+      <Register />
       <Footer />
       <GlobalStyle />
-      </div>
-  
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Register.css";
+
+const Register = () => {
+  return (
+    <div className="registerWrapper">
+      <div className="registerContainer">
+        <div className="registerLeftDiv">Lula</div>
+        <div className="registerRightDiv">Bolsonaro</div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
