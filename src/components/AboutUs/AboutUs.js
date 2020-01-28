3a1412d53f26b4d86b9de536cbc5b76a26100553
ajedelmann <albert.edelmann@gmail.com>
2020-01-28 22:30:25 +0100

    added AboutUs component

---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./AboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div className="wrapper">
+      <div className="topContainer"></div>
+      <div className="bottomContainer"></div>
+      <h2 className="h2AboutUs">
+        "Our Mission is Mussum Ipsum, cacilds vidis litro abertis. Leite de
+        capivaris, leite de mula manquis sem cabeça. Interagi no mé, cursus
+        quis, vehicula ac nisi. Em pé sem cair, deitado sem dormir, sentado sem
+        cochilar e fazendo pose. Si u mundo tá muito paradis? Toma um mé que o
+        mundo vai girarzis!"
+      </h2>
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import Navbar from '../navbar/Navbar';
-import '../navbar/Navbar.css'
-import Jumbotron from '../Jumbotron'
-import '../Jumbotron.css'
-import './App.css'
-import '../../styles/animate.css'
-import Footer from '../Footer'
-import '../Footer.css'
-
-import GlobalStyle from '../../styles/Global'
+import React from "react";
+import Navbar from "../navbar/Navbar";
+import "../navbar/Navbar.css";
+import Jumbotron from "../AboutUs/AboutUs";
+import "./App.css";
+import "../../styles/animate.css";
+import Footer from "../Footer/Footer";
+import GlobalStyle from "../../styles/Global";
 
 const App = () => {
-
   return (
     <div className="MainPage">
       <Navbar />
       <Jumbotron />
       <Footer />
       <GlobalStyle />
-      </div>
-  
+    </div>
   );
-}
+};
 
 export default App;
