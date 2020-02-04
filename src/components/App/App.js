63465c8b84d66375ab0cf78e7c4c183595a2ddf2
ajedelmann <albert.edelmann@gmail.com>
2020-02-04 14:44:50 +0100

    added homepage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import Navbar from '../navbar/Navbar';
-import '../navbar/Navbar.css'
-import Jumbotron from '../Jumbotron'
-import '../Jumbotron.css'
-import './App.css'
-import '../../styles/animate.css'
-import Footer from '../Footer'
-import '../Footer.css'
+import React from "react";
+import Navbar from "../navbar/Navbar";
+import "../navbar/Navbar.css";
+import "./App.css";
+import "../../styles/animate.css";
+import Footer from "../Footer";
+import "../Footer.css";
+import Desktop from "../Desktop/Desktop";
 
-import GlobalStyle from '../../styles/Global'
+import GlobalStyle from "../../styles/Global";
 
 const App = () => {
-
   return (
     <div className="MainPage">
       <Navbar />
-      <Jumbotron />
+      <Desktop />
       <Footer />
       <GlobalStyle />
-      </div>
-  
+    </div>
   );
-}
+};
 
 export default App;
--- src/components/navbar/DropdownMenu/DropdownMenu.js
+++ src/components/navbar/DropdownMenu/DropdownMenu.js
@@ -1,38 +1,59 @@
-import React, { useState } from 'react'
-import './DropdownMenu.css'
-import Image from '../../../images/user.png'
+import React, { useState } from "react";
+import "./DropdownMenu.css";
+import Image from "../../../assets/user.png";
 
 const DropdownMenu = () => {
+  //Here we can set Dropdown Menu to Open or closed
+  const [State, SetState] = useState({ OpenMenu: false });
 
-//Here we can set Dropdown Menu to Open or closed
-const [State, SetState] = useState({OpenMenu: false})
+  // Here ypu can modify the Array to change the items from te dropdown menu
+  const [DropdownItemsList, SetDropdownItemsList] = useState([
+    { Title: "Register", Path: "#" },
+    { Title: "Login/Log Out", Path: "#" },
+    { Title: "My Account", Path: "#" }
+  ]);
 
-// Here ypu can modify the Array to change the items from te dropdown menu
-const [DropdownItemsList, SetDropdownItemsList] = 
-useState([{Title: "Register", Path: '#'}, {Title: "Login/Log Out", Path: '#'}, {Title: "My Account", Path: '#'}])
-
-// Here you can set the Main Title of the Dropdown menu in the return of this function
-const setTitle = () => { 
+  // Here you can set the Main Title of the Dropdown menu in the return of this function
+  const setTitle = () => {
     return (
-        <div className="centered"><img className={State.OpenMenu ? "userImg scaling" : "userImg"} src={Image}/>
-    {!State.OpenMenu ? <span>&#9663;</span> : <span>&#9653;</span>}</div>
-    )
-}
+      <div className="centered">
+        <img
+          className={State.OpenMenu ? "userImg scaling" : "userImg"}
+          src={Image}
+        />
+        {!State.OpenMenu ? <span>&#9663;</span> : <span>&#9653;</span>}
+      </div>
+    );
+  };
 
-const onClick = () => {
-    SetState({OpenMenu: !State.OpenMenu})
-    console.log(State.OpenMenu)
-
-}
-const DropdownItems = DropdownItemsList.map(el =>  
-    (<div className={State.OpenMenu ? "DropdownItemOpen fadeIn" : "DropdownItemClosed"}><a href={el.Path}>{el.Title}</a></div>) )
-return (
-    <div className="DropdownContainer">
-<div className="DropdownTitle" onClick={onClick}>{setTitle()}</div>
-        <div className={State.OpenMenu ? "DropdownHiddenContainerOpen" : "DropdownHiddenContainerClosed"}>
-        {DropdownItems}
-        </div>
+  const onClick = () => {
+    SetState({ OpenMenu: !State.OpenMenu });
+    console.log(State.OpenMenu);
+  };
+  const DropdownItems = DropdownItemsList.map(el => (
+    <div
+      className={
+        State.OpenMenu ? "DropdownItemOpen fadeIn" : "DropdownItemClosed"
+      }
+    >
+      <a href={el.Path}>{el.Title}</a>
     </div>
-)    
-}
+  ));
+  return (
+    <div className="DropdownContainer">
+      <div className="DropdownTitle" onClick={onClick}>
+        {setTitle()}
+      </div>
+      <div
+        className={
+          State.OpenMenu
+            ? "DropdownHiddenContainerOpen"
+            : "DropdownHiddenContainerClosed"
+        }
+      >
+        {DropdownItems}
+      </div>
+    </div>
+  );
+};
 export default DropdownMenu;
