33a68b30b0c06fc6239fe74b73b383c26aab8def
ajedelmann <albert.edelmann@gmail.com>
2020-02-04 21:46:56 +0100

    added Jumbotron at CV_Templates

--- src/components/CV_Templates/CV_Templates.js
+++ src/components/CV_Templates/CV_Templates.js
@@ -7,29 +7,33 @@ import CV4 from "../../assets/CV4.png";
 import CV5 from "../../assets/CV5.jpg";
 import CV6 from "../../assets/CV6.jpg";
 import ScrollAnimation from "react-animate-on-scroll";
+import Jumbotron from "./Jumbotron";
 
 const CV_Templates = () => {
   return (
-    <div className="CV_Templates_MainContainer">
-      <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-0s">
-        <img src={CV1} />
-      </ScrollAnimation>
-      <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-1s">
-        <img src={CV2} />
-      </ScrollAnimation>
-      <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-2s">
-        <img src={CV3} />
-      </ScrollAnimation>
-      <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-0s">
-        <img src={CV4} />
-      </ScrollAnimation>
-      <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-1s">
-        <img src={CV5} />
-      </ScrollAnimation>
-      <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-2s">
-        <img src={CV6} />
-      </ScrollAnimation>
-    </div>
+    <>
+      <Jumbotron />
+      <div className="CV_Templates_MainContainer">
+        <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-0s">
+          <img src={CV1} />
+        </ScrollAnimation>
+        <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-1s">
+          <img src={CV2} />
+        </ScrollAnimation>
+        <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-2s">
+          <img src={CV3} />
+        </ScrollAnimation>
+        <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-0s">
+          <img src={CV4} />
+        </ScrollAnimation>
+        <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-1s">
+          <img src={CV5} />
+        </ScrollAnimation>
+        <ScrollAnimation className="cvBox" animateIn="fadeInUp delay-2s">
+          <img src={CV6} />
+        </ScrollAnimation>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/CV_Templates/Jumbotron.js
@@ -0,0 +1,28 @@
+import React from "react";
+import JumboPic from "../../assets/JumboPic2.png";
+import "../../styles/Jumbotron.css";
+
+const Jumbotron = () => {
+  return (
+    <div className="Jumbotron">
+      <div className="JumboText">
+        <div className="JumboTextBox">
+          <h1 className="animated fadeInLeft">Here will be a Cool Title</h1>
+          <p className="animated fadeInUp delay-1s">
+            Your CV is the first point of contact between you and your next
+            potential employer. It is important that it is succinct and
+            presented in an easy to read format using a simple font, minimal
+            styling and the use of bullet points to break down information. To
+            ensure your CV stands out in your next job application follow our
+            recommended structure and tips below:
+          </p>
+        </div>
+      </div>
+      <div className="JumboPic">
+        <img className="animated fadeIn delay-1s" src={JumboPic} />
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
