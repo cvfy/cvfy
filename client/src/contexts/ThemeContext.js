55498ab605faa095207e2e948feb2fa5aa5e78b1
ajedelmann <albert.edelmann@gmail.com>
2020-02-19 16:44:20 +0100

    added dynamic contacts

--- client/src/contexts/ThemeContext.js
+++ client/src/contexts/ThemeContext.js
@@ -1,10 +1,9 @@
 import React, { Component, createContext } from "react";
 import SkillBox from "../components/CvBuilder/CvBuilderMain/Skills/SkillBox";
-import axios from 'axios';
+import axios from "axios";
 
 // import { response } from "express";
 export const ThemeContext = createContext();
-
 
 // const importDatata = async () => {
 // const impdata = await axios.get("localhost:5000/api/users/data/bleda-hacialihafiz")
@@ -26,12 +25,13 @@ class ThemeContextProvider extends Component {
       intro: "Frontend Developer",
       about: "I like apple, bacon and mushrooms",
       contact: [
-        { email: "steve.jobs@apple.com" },
-        { phone: "0170 999888888" },
-        { linkedIn: "linkedin.com/in/steve.jobs" },
-        { skype: "steve.jobs" },
-        { address: "Kingston, Jamaica" },
-        { website: "https://ajedelmann.github.io/portfolio/" }
+        { icon: "far fa-envelope", value: "" },
+        { icon: "fas fa-mobile-alt", value: "00000000" },
+        { icon: "fab fa-linkedin", value: "linkedin" },
+        { icon: "fab fa-skype", value: "skype" },
+        { icon: "fas fa-map-marker-alt", value: "" },
+        { icon: "fas fa-globe", value: "" },
+        { icon: "fab fa-github", value: "" }
       ],
       experience: [
         {
@@ -80,55 +80,61 @@ class ThemeContextProvider extends Component {
     }
   };
 
-  importData = async (profile) => {
+  importData = async profile => {
     // console.log("hahahha")
     // const respo = await importDatata()
     // console.log(respo)
-//     debugger
-//     fetch(`http://localhost:5000/api/users/data/vladharagea/`)
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(json) {
-//     console.log(json);
-//   });
-// };
-      const response = await axios.get(`http://localhost:5000/api/users/data/${profile}`)
-      console.log(response.data)
-      let newObject = { ...this.state.userData };
-      newObject.fullName = response.data.profileFullName;
-      newObject.intro = response.data.profileHeadline;
-      newObject.about = response.data.profileAbout[0];
-      newObject.skills = response.data.skills;
-      newObject.linkedIn =`linkedin.com/in/${profile}`;
-      newObject.experience = response.data.profileExperience.map( el => {
-        let new_el = {}
-        new_el.position = el.jobTitle;
-        new_el.company = el.jobEmployer;
-        new_el.startMonth = el.jobPeriod.split(" ")[0] || "";
-        new_el.startYear = el.jobPeriod.split(" ")[1] || "";
-        new_el.endMonth = el.jobPeriod.split(" ")[3] || "";
-        new_el.endYear = el.jobPeriod.split(" ")[4] || "";
-        new_el.place = el.jobLocation;
-        new_el.tasks = el.jobDescription;
-        return new_el
-      })
-      newObject.education = response.data.profileEducation.map( el => {
-        let new_el = {}
-        new_el.studyProgram = el.educationType;
-        new_el.institution = el.educationInstitution;
-        new_el.startMonth = "";
-        new_el.startYear = el.educationPeriod.split(" ")[0]
-        new_el.endMonth = "";
-        new_el.endYear = el.educationPeriod.split(" ")[2] || "";
-        new_el.place = ""
-        return new_el
-      })
-      newObject.languages = response.data.accomplishments[0].accomplishmentList.map(el =>  { return {language: el.split("\n")[1], level: "B2"}})
-      this.setState({ userData: newObject });
+    //     debugger
+    //     fetch(`http://localhost:5000/api/users/data/vladharagea/`)
+    //   .then(function(response) {
+    //     return response.json();
+    //   })
+    //   .then(function(json) {
+    //     console.log(json);
+    //   });
+    // };
+    const response = await axios.get(
+      `http://localhost:5000/api/users/data/${profile}`
+    );
+    console.log(response.data);
+    let newObject = { ...this.state.userData };
+    newObject.fullName = response.data.profileFullName;
+    newObject.intro = response.data.profileHeadline;
+    newObject.about = response.data.profileAbout[0];
+    newObject.skills = response.data.skills;
+    newObject.linkedIn = `linkedin.com/in/${profile}`;
+    newObject.experience = response.data.profileExperience.map(el => {
+      let new_el = {};
+      new_el.position = el.jobTitle;
+      new_el.company = el.jobEmployer;
+      new_el.startMonth = el.jobPeriod.split(" ")[0] || "";
+      new_el.startYear = el.jobPeriod.split(" ")[1] || "";
+      new_el.endMonth = el.jobPeriod.split(" ")[3] || "";
+      new_el.endYear = el.jobPeriod.split(" ")[4] || "";
+      new_el.place = el.jobLocation;
+      new_el.tasks = el.jobDescription;
+      return new_el;
+    });
+    newObject.education = response.data.profileEducation.map(el => {
+      let new_el = {};
+      new_el.studyProgram = el.educationType;
+      new_el.institution = el.educationInstitution;
+      new_el.startMonth = "";
+      new_el.startYear = el.educationPeriod.split(" ")[0];
+      new_el.endMonth = "";
+      new_el.endYear = el.educationPeriod.split(" ")[2] || "";
+      new_el.place = "";
+      return new_el;
+    });
+    newObject.languages = response.data.accomplishments[0].accomplishmentList.map(
+      el => {
+        return { language: el.split("\n")[1], level: "B2" };
+      }
+    );
+    this.setState({ userData: newObject });
 
-      // axios.get("localhost:5000/api/users/data/bleda-hacialihafiz").then(res => console.log(res.data))
-  }
+    // axios.get("localhost:5000/api/users/data/bleda-hacialihafiz").then(res => console.log(res.data))
+  };
 
   // Those 3 functions add array of strings, will try to DRY later
   addSkillGroup = () => {
@@ -185,6 +191,11 @@ class ThemeContextProvider extends Component {
     };
     newObject.education = [...this.state.userData.education, newEducation];
     this.setState({ userData: newObject });
+  };
+
+  handleContactIcon = () => {
+    let element = document.getElementsByClassName("iconeColor");
+    element.classList.add(this.state.userData.contact.icone);
   };
 
   addLanguageGroup = () => {
