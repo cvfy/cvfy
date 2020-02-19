8dfa9cc234f166a79d6ac7a8a5c16a16f861f7b4
ajedelmann <albert.edelmann@gmail.com>
2020-02-19 13:36:25 +0100

    added saveUpdateButton and improved LinkedInData component style

--- client/src/components/CvBuilder/CvBuilderNav/LinkedInData.js
+++ client/src/components/CvBuilder/CvBuilderNav/LinkedInData.js
@@ -2,16 +2,15 @@ import React, { Component } from "react";
 import "../../../styles/BuilderNav.css";
 import { ThemeContext } from "../../../contexts/ThemeContext";
 
-
 class LinkedInData extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = { value: "" };
 
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   render() {
@@ -19,12 +18,23 @@ class LinkedInData extends Component {
       <ThemeContext.Consumer>
         {context => {
           const { importData } = context;
-        return (
-      <div className="cvExtraToolsDiv">
-        <input value={this.state.value} onChange={this.handleChange} id="linkedInInput" placeholder="insert your linkedIn"></input>
-        <button onClick={()=> importData(this.state.value) } className="linkedInButton">Import</button>
-      </div>
-        )
+          return (
+            <div className="cvExtraToolsDiv">
+              <input
+                value={this.state.value}
+                onChange={this.handleChange}
+                id="linkedInInput"
+                placeholder="insert your linkedIn account"
+              ></input>
+              <button
+                onClick={() => importData(this.state.value)}
+                className="linkedInButton"
+              >
+                Import
+              </button>
+              <button className="saveUpdateButton">Save</button>
+            </div>
+          );
         }}
       </ThemeContext.Consumer>
     );
