ee69cde7907ce85a16e6bec53594e47757f9cf9a
ajedelmann <albert.edelmann@gmail.com>
2020-03-09 14:41:53 +0100

    redesigned cover builder and cv builder navs, added undo redo features, new doc buttons and save button at cover

--- client/src/components/CoverLetterBuilder/CoverLetterNav/ExtraToolsMenu.js
+++ client/src/components/CoverLetterBuilder/CoverLetterNav/ExtraToolsMenu.js
@@ -15,131 +15,135 @@ class ExtraToolsMenu extends Component {
             toggleJustifyLeft,
             toggleJustifyRight,
             toggleInsertOrderedList,
-            toggleInsertUnorderedList,
             toggleUndo,
-            toggleRedo
+            toggleRedo,
+            saveCVDataToServer
           } = context;
           return (
             <div className="cvExtraToolsDiv">
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool boldStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleFontWeight}
-                    title="bold"
-                  >
-                    B
-                  </span>
+              <div className="toolsDiv">
+                <div className="extra-tools-btn">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool boldStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleFontWeight}
+                      title="bold"
+                    >
+                      B
+                    </span>
+                  </div>
+                </div>
+                <div className="extra-tools-btn">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool italicStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleFontStyle}
+                      title="italic"
+                    >
+                      I
+                    </span>
+                  </div>
+                </div>
+                <div className="extra-tools-btn">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool underStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleTextDecoration}
+                      title="underline"
+                    >
+                      <i className="fas fa-underline"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="extra-tools-btn">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool leftStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleJustifyLeft}
+                      title="justify-left"
+                    >
+                      <i className="fas fa-align-left"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="extra-tools-btn">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool centerStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleJustifyCenter}
+                      title="justify-center"
+                    >
+                      <i className="fas fa-align-center"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="extra-tools-btn">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool rightStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleJustifyRight}
+                      title="justify-right"
+                    >
+                      <i className="fas fa-align-right"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="extra-tools-btn">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool olStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleInsertOrderedList}
+                      title="ordered list"
+                    >
+                      <i className="fas fa-list-ol"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="extra-tools-btn">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool olStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleUndo}
+                      title="undo"
+                    >
+                      <i class="fas fa-undo-alt"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="extra-tools-btn">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool olStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleRedo}
+                      title="redo"
+                    >
+                      <i class="fas fa-redo-alt"></i>
+                    </span>
+                  </div>
                 </div>
               </div>
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool italicStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleFontStyle}
-                    title="italic"
-                  >
-                    I
-                  </span>
-                </div>
-              </div>
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool underStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleTextDecoration}
-                    title="underline"
-                  >
-                    <i className="fas fa-underline"></i>
-                  </span>
-                </div>
-              </div>
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool leftStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleJustifyLeft}
-                    title="justify-left"
-                  >
-                    <i className="fas fa-align-left"></i>
-                  </span>
-                </div>
-              </div>
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool centerStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleJustifyCenter}
-                    title="justify-center"
-                  >
-                    <i className="fas fa-align-center"></i>
-                  </span>
-                </div>
-              </div>
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool rightStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleJustifyRight}
-                    title="justify-right"
-                  >
-                    <i className="fas fa-align-right"></i>
-                  </span>
-                </div>
-              </div>
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool ulStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleInsertUnorderedList}
-                    title="bullet list"
-                  >
-                    <i className="fas fa-list-ul"></i>
-                  </span>
-                </div>
-              </div>
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool olStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleInsertOrderedList}
-                    title="ordered list"
-                  >
-                    <i className="fas fa-list-ol"></i>
-                  </span>
-                </div>
-              </div>
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool olStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleUndo}
-                    title="undo"
-                  >
-                    <i class="fas fa-undo-alt"></i>
-                  </span>
-                </div>
-              </div>
-              <div className="extra-tools-btn">
-                <div className="docsDiv">
-                  <span
-                    className="tool-icon extra-tool olStyle"
-                    onMouseDown={e => e.preventDefault()}
-                    onClick={toggleRedo}
-                    title="redo"
-                  >
-                    <i class="fas fa-redo-alt"></i>
-                  </span>
-                </div>
+              <div className="buttonsDiv">
+                <button
+                  onClick={e => saveCVDataToServer(e)}
+                  className="saveUpdateButton"
+                >
+                  Save
+                </button>
+                <button
+                  // onClick={e => saveCVDataToServer(e)}
+                  className="saveUpdateButton"
+                >
+                  New doc
+                </button>
               </div>
             </div>
           );
--- client/src/components/CvBuilder/CvBuilderNav/LinkedInData.js
+++ client/src/components/CvBuilder/CvBuilderNav/LinkedInData.js
@@ -17,22 +17,170 @@ class LinkedInData extends Component {
     return (
       <ThemeContext.Consumer>
         {context => {
-          const { importData, saveCVDataToServer, generatePDF } = context;
+          const {
+            importData,
+            saveCVDataToServer,
+            generatePDF,
+            toggleFontWeight,
+            toggleFontStyle,
+            toggleTextDecoration,
+            toggleJustifyLeft,
+            toggleJustifyCenter,
+            toggleJustifyRight,
+            toggleInsertOrderedList,
+            toggleUndo,
+            toggleRedo
+          } = context;
           return (
-            <div className="cvExtraToolsDiv">
-              <input
-                value={this.state.value}
-                onChange={this.handleChange}
-                id="linkedInInput"
-                placeholder="insert your linkedIn account"
-              ></input>
-              <button
-                onClick={() => importData(this.state.value)}
-                className="linkedInButton"
-              >
-                Import
-              </button>
-              <button onClick={(e) => saveCVDataToServer(e)} className="saveUpdateButton">Save</button>
+            // <div className="cvExtraToolsDiv">
+            //   <input
+            //     value={this.state.value}
+            //     onChange={this.handleChange}
+            //     id="linkedInInput"
+            //     placeholder="insert your linkedIn account"
+            //   ></input>
+            //   <button
+            //     onClick={() => importData(this.state.value)}
+            //     className="linkedInButton"
+            //   >
+            //     Import
+            //   </button>
+            //   <button onClick={(e) => saveCVDataToServer(e)} className="saveUpdateButton">Save</button>
+            // </div>
+
+            <div className="menuBottomBarDiv">
+              <div className="toolsDiv">
+                <div className="cvBuilder-extra-tools">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool boldStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleFontWeight}
+                      title="bold"
+                    >
+                      B
+                    </span>
+                  </div>
+                </div>
+                <div className="cvBuilder-extra-tools">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool italicStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleFontStyle}
+                      title="italic"
+                    >
+                      I
+                    </span>
+                  </div>
+                </div>
+                <div className="cvBuilder-extra-tools">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool underStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleTextDecoration}
+                      title="underline"
+                    >
+                      <i className="fas fa-underline"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="cvBuilder-extra-tools">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool leftStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleJustifyLeft}
+                      title="justify-left"
+                    >
+                      <i className="fas fa-align-left"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="cvBuilder-extra-tools">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool centerStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleJustifyCenter}
+                      title="justify-center"
+                    >
+                      <i className="fas fa-align-center"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="cvBuilder-extra-tools">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool rightStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleJustifyRight}
+                      title="justify-right"
+                    >
+                      <i className="fas fa-align-right"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="cvBuilder-extra-tools">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool olStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleInsertOrderedList}
+                      title="ordered list"
+                    >
+                      <i className="fas fa-list-ol"></i>
+                    </span>
+                  </div>
+                </div>
+
+                <div className="cvBuilder-extra-tools">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool olStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleUndo}
+                      title="undo"
+                    >
+                      <i class="fas fa-undo-alt"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="cvBuilder-extra-tools">
+                  <div className="docsDiv">
+                    <span
+                      className="tool-icon extra-tool olStyle"
+                      onMouseDown={e => e.preventDefault()}
+                      onClick={toggleRedo}
+                      title="redo"
+                    >
+                      <i class="fas fa-redo-alt"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="buttonsDiv">
+                <button
+                  onClick={() => importData(this.state.value)}
+                  className="linkedInButton"
+                >
+                  Import
+                </button>
+                <button
+                  onClick={e => saveCVDataToServer(e)}
+                  className="saveUpdateButton"
+                >
+                  Save
+                </button>
+                <button
+                  // onClick={e => saveCVDataToServer(e)}
+                  className="saveUpdateButton"
+                >
+                  New CV
+                </button>
+              </div>
             </div>
           );
         }}
