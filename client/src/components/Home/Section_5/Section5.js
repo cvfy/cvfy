cc792304d1ae58095d948abcdf384b6aafb22b37
vladharagea <vladharagea@yahoo.com>
2020-03-10 13:56:03 +0100

    added landintPage Main pic

---
+++ client/src/components/Home/Section_5/Section5.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+
+const Section5 = () => {
+  return (
+    <div className="LPContainer BlueTintBg VideoContainer">
+      <iframe
+        className="IframeContainer"
+        src="https://fast.wistia.net/embed/iframe/lcu5575a21"
+      />
+    </div>
+  );
+};
+export default Section5;
