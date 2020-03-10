006781722f370fff72ba494c05bb83066028cf0e
vladharagea <vladharagea@yahoo.com>
2020-03-10 14:42:29 +0100

    fixed MyDocuments Button

--- client/src/components/MyDocuments/MyDocuments.js
+++ client/src/components/MyDocuments/MyDocuments.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "../navbar/Navbar";
-import "../../styles/myDocuments.css";
+import "../../styles/MyDocuments.scss";
 import CvDashboard from "./CvDashboard";
 import CoverDashboard from "./CoverDashboard";
 
