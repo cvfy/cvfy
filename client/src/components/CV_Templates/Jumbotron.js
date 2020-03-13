1bb99b4f242c9d8b6c66f5335a26b693df55abf6
vladharagea <vladharagea@yahoo.com>
2020-03-13 12:58:00 +0100

    added templates.scss for template sites

--- client/src/components/CV_Templates/Jumbotron.js
+++ client/src/components/CV_Templates/Jumbotron.js
@@ -1,14 +1,14 @@
 import React from "react";
 import JumboPic from "../../assets/JumboPic2.png";
-import "../../styles/Jumbotron.css";
+import "../../styles/Templates.scss";
 
 const Jumbotron = () => {
   return (
     <div className="Jumbotron">
       <div className="JumboText">
         <div className="JumboTextBox">
-          <h1 className="animated fadeInLeft">Here will be a Cool Title</h1>
-          <p className="animated fadeInUp delay-1s">
+          <h1 className="animated fadeIn">Here will be a Cool Title</h1>
+          <p className="animated fadeIn">
             Your CV is the first point of contact between you and your next
             potential employer. It is important that it is succinct and
             presented in an easy to read format using a simple font, minimal
@@ -19,7 +19,7 @@ const Jumbotron = () => {
         </div>
       </div>
       <div className="JumboPic">
-        <img className="animated fadeIn delay-1s" src={JumboPic} />
+        <img className="animated fadeIn" src={JumboPic} />
       </div>
     </div>
   );
