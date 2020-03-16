23940ce09f1a7580566dbbc1524146699b38f387
vladharagea <vladharagea@yahoo.com>
2020-03-16 14:20:26 +0100

    cleaned Template Pages

--- client/src/components/CV_Templates/Jumbotron.js
+++ client/src/components/CV_Templates/Jumbotron.js
@@ -4,21 +4,19 @@ import "../../styles/Templates.scss";
 
 const Jumbotron = () => {
   return (
-    <div className="Jumbotron">
-      <div className="JumboText">
-        <div className="JumboTextBox">
-          <h1 className="animated fadeIn">Here will be a Cool Title</h1>
-          <p className="animated fadeIn">
-            Your CV is the first point of contact between you and your next
-            potential employer. It is important that it is succinct and
-            presented in an easy to read format using a simple font, minimal
-            styling and the use of bullet points to break down information. To
-            ensure your CV stands out in your next job application follow our
-            recommended structure and tips below:
-          </p>
-        </div>
+    <div className="JumbotronCVTemplates">
+      <div className="JumboLeft">
+        <h1>Here will be a Cool Title</h1>
+        <p>
+          Your CV is the first point of contact between you and your next
+          potential employer. It is important that it is succinct and presented
+          in an easy to read format using a simple font, minimal styling and the
+          use of bullet points to break down information. To ensure your CV
+          stands out in your next job application follow our recommended
+          structure and tips below:
+        </p>
       </div>
-      <div className="JumboPic">
+      <div className="JumboRight">
         <img className="animated fadeIn" src={JumboPic} />
       </div>
     </div>
