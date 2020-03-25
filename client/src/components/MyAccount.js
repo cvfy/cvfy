b675f197f9aaf86fe2cd379801a0715ce147b366
vladharagea <vladharagea@yahoo.com>
2020-03-25 13:40:17 +0100

    fixed contactUs heights and My Account page

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -2,8 +2,7 @@ import React from "react";
 import Navbar from "./navbar/Navbar";
 import Footer from "./Footer";
 import ProfilePicture from "../assets/user.png";
-import ScrollAnimation from "react-animate-on-scroll";
-import "../styles/myAccount.css";
+import "../styles/myAccount.scss";
 import store from "./../store";
 
 function aFunction(type) {
@@ -16,72 +15,56 @@ const MyAccount = () => {
       <Navbar />
       <div className="Jumbotron">
         <div className="ProfilePicContainer">
-          <ScrollAnimation animateIn="fadeIn delay-0s">
-            <div className="ProfilePic">
-              <img src={ProfilePicture} />
-            </div>
-          </ScrollAnimation>
-          <ScrollAnimation animateIn="fadeInUp delay-1s">
-            <h1>{aFunction("name")}</h1>
-          </ScrollAnimation>
+          <div className="ProfilePic">
+            <img src={ProfilePicture} />
+          </div>
+          <h1>{aFunction("name")}</h1>
         </div>
         <div className="ProfileData">
-          <ScrollAnimation
-            className="ProfileMainData"
-            animateIn="fadeInRight delay-0s"
-          >
-            <ScrollAnimation className="FieldRow" animateIn="fadeIn delay-1s">
-              <h1>Account Details</h1>
-            </ScrollAnimation>
+          <div className="ProfileMainData">
             <div className="FieldRow">
-              <ScrollAnimation className="field" animateIn="fadeInUp delay-1s">
+              <h1>Account Details</h1>
+            </div>
+            <div className="FieldRow">
+              <div className="field">
                 <label>Full Name</label>
                 <div className="FirstNameField">{aFunction("name")}</div>
-              </ScrollAnimation>
-              <ScrollAnimation className="field" animateIn="fadeInUp delay-1s">
+              </div>
+              <div className="field">
                 <label>Email</label>
                 <div className="EmailField">{aFunction("email")}</div>
-              </ScrollAnimation>
+              </div>
             </div>
             <div className="FieldRow">
-              <ScrollAnimation className="field" animateIn="fadeInUp delay-1s">
-              <label>Password</label>
-                <div className="PasswordField">**********</div>
-              </ScrollAnimation>
-              <ScrollAnimation className="field" animateIn="fadeInUp delay-1s">
+              <div className="field">
                 <label>Password</label>
                 <div className="PasswordField">**********</div>
-              </ScrollAnimation>
+              </div>
+              <div className="field">
+                <label>Password</label>
+                <div className="PasswordField">**********</div>
+              </div>
             </div>
-            <div className="FieldRow">
-              <ScrollAnimation className="field" animateIn="fadeInUp delay-1s">
+            <div>
+              <div className="field">
                 <a className="linkField" href="#">
                   View my stored Personal Data
                 </a>
-              </ScrollAnimation>
-              <ScrollAnimation className="field" animateIn="fadeInUp delay-1s">
-                <a className="linkField" href="#">
-                  Deleted My Account
-                </a>
-              </ScrollAnimation>
-            </div>
-            <ScrollAnimation
-              className="checkboxField"
-              animateIn="fadeInUp delay-1s"
-            >
-              <div>
-                <input type="checkbox" />
               </div>
+              <div className="field">
+                <a className="linkField" href="#">
+                  Delete my Account
+                </a>
+              </div>
+            </div>
+            <div className="checkboxField">
               <h3>I want to receive email notifications from CV|FY</h3>
-            </ScrollAnimation>
-          </ScrollAnimation>
-          <ScrollAnimation
-            className="ProfileMainDataSmall"
-            animateIn="fadeInRight delay-1s"
-          ></ScrollAnimation>
+              <input type="checkbox" />
+            </div>
+          </div>
         </div>
       </div>
-      
+
       <Footer></Footer>
     </>
   );
