b3ae1b5f619cac690d91306f0e899698548970ee
AJEdelmann <47785661+AJEdelmann@users.noreply.github.com>
2020-03-26 19:17:37 +0100

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,10 @@
-// export const url = "http://localhost:5000";
-export const url = "http://ec2-18-191-117-123.us-east-2.compute.amazonaws.com";
+export const url = "http://localhost:5000";
+// export const url = "http://ec2-18-191-117-123.us-east-2.compute.amazonaws.com";
 
 export const cloudinaryUrl =
   "https://api.cloudinary.com/v1_1/dtox6ulq7/image/upload";
 
-export const frontUrl =
-  "http://ec2-18-191-117-123.us-east-2.compute.amazonaws.com";
+//export const frontUrl =
+//  "http://ec2-18-191-117-123.us-east-2.compute.amazonaws.com";
 
-// export const frontUrl = "http://localhost:3000";
+ export const frontUrl = "http://localhost:3000";
