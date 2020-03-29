ea5d9f7e2ee879d28ad527173919cdfed7414aaf
AlexandrMel <melnic.alexandr88@gmail.com>
2020-03-29 20:48:53 +0200

    reduset jobo Dashboard loading time to 9-10 seconds

--- puppeteer_Data/StepStoneData.js
+++ puppeteer_Data/StepStoneData.js
@@ -9,8 +9,8 @@ async function giveMeJobData(position, location, pages) {
         try {
             const page = await browser.newPage()
             await page.setViewport({
-                width: 1200,
-                height: 1600
+                width: 1000,
+                height: 1200
             })
             console.log("calling step1")
             await page.goto(`https://www.stepstone.de/5/job-search-simple.html?stf=freeText&ns=1&companyid=0&sourceofthesearchfield=resultlistpage%3Ageneral&qs=%5B%5D&ke=${position}&ws=${location}&ra=30&suid=a2c2ed4a-e81e-44c2-bf24-afcd6383ec87&ob=relevance&action=sort_relevance`);
@@ -26,8 +26,11 @@ async function giveMeJobData(position, location, pages) {
             let start = 0 + parseInt(pages)
             let end = 4 + parseInt(pages)
             for(i=start; i<end; i++){           
-            await page.goto(`https://www.stepstone.de${jobData[i]}`);
-            let Data = await page.evaluate((jobData) => {
+            await page.goto(`https://www.stepstone.de${jobData[i]}`, {
+                waitUntil: "domcontentloaded"
+            });
+
+            let Data = await page.evaluate(() => {
                 function verify(data) {
                     return (data !== undefined && data !== null) ? data : ""
                 }
