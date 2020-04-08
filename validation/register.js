9d2373a64bca78ddf9983608b5cca113f2fbb0b3
AlexandrMel <melnic.alexandr88@gmail.com>
2020-04-08 12:22:36 +0200

    Cleaned and commented validation/register.js file

--- validation/register.js
+++ validation/register.js
@@ -1,40 +1,46 @@
+// IMPORTED PACKAGES
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
+// EXPORTING THE VALIDATION FUNCTION THAT TAKES AS ARGUMENT FORM DATA FROM THE REQEST BODY AND DOES THE VALIDATIONS
 module.exports = function validateRegisterInput(data) {
-    let errors = {};
-//Convert empty fields to an empty string so we can use validator functions
 
+// CREATE EMPTY OBJECT TO STORE THE ERRORS RECEIVED FROM VALIDATORS
+    let errors = {};
+
+// CONVERT EMPTY FIELDS TO AN EMPTY STRING SO WE CAN USE VALIDATOR FUNCTIONS
 data.name = !isEmpty(data.name) ? data.name : "";
 data.email = !isEmpty(data.email) ? data.email : "";
 data.password = !isEmpty(data.password) ? data.password : "";
 data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-//Name checks
+//NAME CHECKS(ONLY CHECKS IF THE FIELD IS EMPTY, NO OTHER CHECKS IMPLEMENTED)
 if(Validator.isEmpty(data.name)) {
     errors.name = 'Name field in required';
 }
 
-//Email checks
+//EMAIL CHECKS(CHECKS IF THE FIELD IS EMPTY AND IF IT IS IS A VALID EMAIL)
 if(Validator.isEmpty(data.email)) {
     errors.email = 'Email field in required';
 } else  if(!Validator.isEmail(data.email)){
     errors.email = "Email is invalid";
 }
 
-//Password checks
+//PASSWORD CHECKS(CHECKS IF BOTH PASSWORD FIELDS ARE EMPTY, SETS A LENGTH REQUIREMENT FOR THE PASSWORD AND CHECKS IF PASSWORD FIELDS MATCH)
 if(Validator.isEmpty(data.password)){
     errors.password = "Password field is required";
 }
 if(Validator.isEmpty(data.password2)){
     errors.password = "Confirm password field is required";
 }
-if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+if (!Validator.isLength(data.password, { min: 6, max: 30 })) // <--- YOU CAN ADJUST LENGTH REQUIREMENTS HERE
+{
     errors.password = "Password must be at least 6 characters";
   }if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = "Passwords must match";
   }
 
+// WE RETURN ALL GATHERED ERRORS AS ERROR OBJECT THAT IS PASSED TO BE DISPLAYED ON THE FRONT END
 return  {
     errors,
     isValid: isEmpty(errors)
