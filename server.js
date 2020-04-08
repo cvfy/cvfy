3c92ec92ebf7daade2c037fd1612332e0057f57f
AlexandrMel <melnic.alexandr88@gmail.com>
2020-04-08 11:59:35 +0200

    Cleaned and commented server.js file

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
+// IMPORTED PACKAGES
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const cors = require("cors");
 
+// IMPORT USERS ROUTE
 const users = require("./routes/api/users");
 
+// INITIALIZE EXPRESS
 const app = express();
-//Bodyparser middleware
 
+
+// SETTING STATIC SHARED FOLDERS
 app.use("/static", express.static("profile_picture"));
 app.use("/static2", express.static("puppeteer_Data/profile_picture"));
+
+
+//BODY PARSER MIDDLEWARE
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
-//CORS
 
+//ADDING CORS
 app.use(
   cors({
     origin: ["http://localhost:3000"],
@@ -26,24 +33,27 @@ app.use(
   })
 );
 
-//DB Config
-
+//DATABASE CONFIG
 const db = require("./config//keys").mongoURI;
 
-//Connect to MongoDB
+//CONNECT TO MongoDB
 mongoose.set("useFindAndModify", false);
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Database connected successfull!"))
   .catch(err => console.log(err));
 
-// Passport middleware
+// PASSPORT MIDDLEWARE
 app.use(passport.initialize()); // Passport config
 require("./config/passport")(passport); // Routes
+
+// MAIN ROUTE PATH
 app.use("/api/users", users);
 
+// SETTING SERVER PORT
 const port = process.env.PORT || 5000;
 
+// SETTING LISTENER FOR SERVER PORT 
 app.listen(port, () =>
   console.log(`Server up and successfully running on port ${port} !`)
 );
