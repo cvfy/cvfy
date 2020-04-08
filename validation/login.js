1f009b21f484bce87052c2a16a0ae742f4b917bc
AlexandrMel <melnic.alexandr88@gmail.com>
2020-04-08 12:38:47 +0200

    Cleaned and Commented validation/login.js

--- validation/login.js
+++ validation/login.js
@@ -1,17 +1,24 @@
+// IMPORTED PACKAGES
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
+// EXPORTING THE VALIDATION FUNCTION FOR LOGIN ROUTE THAT TAKES AS ARGUMENT FORM DATA FROM THE REQEST BODY AND DOES THE VALIDATIONS
 module.exports = function validateLoginInput(data) {
+
+  // CREATE EMPTY OBJECT TO STORE THE ERRORS RECEIVED FROM VALIDATORS
   let errors = {};
   
-  // Convert empty fields to an empty string so we can use validator functions
+// CONVERT EMPTY FIELDS TO AN EMPTY STRING SO WE CAN USE VALIDATOR FUNCTIONS
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";// Email checks
+
+//EMAIL CHECKS(CHECKS IF THE FIELD IS EMPTY AND IF IT IS IS A VALID EMAIL)
   if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required";
   } else if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
-  }// Password checks
+  }
+//PASSWORD CHECKS(CHECKS IF PASSWORD FIELD IS EMPTY)
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";
   }return {
